983e0426af4b1dc698b791145c84c4b3b10b9a5e
Sanjukta125 <97775853+Sanjukta125@users.noreply.github.com>
2025-08-29 10:40:56 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-grocery = ["bread", "milk", "eggs", "butter", "buns", "curd"]
-
-print("grocery list:", grocery)
-grocery_upper = [item.upper() for item in grocery]
-
-print("Uppercase grocery list:", grocery_upper)
