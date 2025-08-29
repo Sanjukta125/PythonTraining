969dc5f17c8686ff6f9d612cf22770607036e70d
Sanjukta Mandal <sanjuktamandal02@gmail.com>
2025-08-29 10:38:06 +0530

    code added

---
+++ hello.py
@@ -0,0 +1,6 @@
+grocery = ["bread", "milk", "eggs", "butter", "buns", "curd"]
+
+print("grocery list:", grocery)
+grocery_upper = [item.upper() for item in grocery]
+
+print("Uppercase grocery list:", grocery_upper)
